8a50b91c4cf19d47373f240ed98b843ac4e87a41
Deep-of-Machine <tjgus5569@hanyang.ac.kr>
2023-08-03 02:15:12 -0700

    Modified Lidar clustering and downsamping by ksh

--- build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/hsh/Self-Driving-Car-Cone-Keeping-Assistance-System/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
