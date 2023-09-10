d5b74ee1d910deef7e663dc2211d747c80f3bfd0
Depp-of-Machine <tjgus5569@hanyang.ac.kr>
2023-09-10 06:40:45 +0000

    ..

--- src/lidar_pkg/src/stanley_gps.py
+++ src/lidar_pkg/src/stanley_gps.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import rospy
 import math
 import numpy as np
@@ -11,10 +13,11 @@ class StanleyControlNode:
         self.gps_lat = None
         self.gps_lon = None
         self.v = 5.0  # 초기 속도
-        self.max_steering_angle = 0.52  # 라디안, 최대 조향각
+        self.max_steering_angle = 0.35  # 라디안, 최대 조향각
         self.k = 0.5  # Stanley controller tuning parameter
-        self.path_x = []  # Placeholder for path's x-coordinates
-        self.path_y = []  # Placeholder for path's y-coordinates
+        self.path_x = [ 37.3014406, 37.301440299999996, 37.301438, 37.3014327, 37.3014248, 37.3014149, 37.3014033, 37.3013897, 37.3013747, 37.3013596, 37.3013443, 37.3013323, 37.3013203, 37.3013068, 37.3012922, 37.3012763, 37.3012619, 37.301246899999995, 37.301229899999996, 37.3012114, 37.3011921, 37.3011732, 37.3011541, 37.3011377, 37.301128999999996, 37.3011228, 37.3011149, 37.301104599999995, 37.3010884, 37.3010704, 37.3010532, 37.3010351, 37.3010174, 37.3009993, 37.300982, 37.300971499999996, 37.3009699]
+        self.path_y = [ 126.8347268, 126.8347266, 126.8347251, 126.83472069999999, 126.8347129, 126.83470279999999, 126.83469149999999, 126.8346796, 126.8346684, 126.8346605, 126.83466, 126.83466659999999, 126.8346744, 126.8346838, 126.83469269999999, 126.83469299999999, 126.8346859, 126.834678, 126.8346711, 126.83466259999999, 126.8346531, 126.8346421, 126.83463139999999, 126.834616, 126.8345935, 126.83456969999999, 126.8345459, 126.8345236, 126.83450939999999, 126.83450389999999, 126.83449429999999, 126.8344826, 126.8344707, 126.83445979999999, 126.83444879999999, 126.8344422, 126.8344412]
+
 
         # UTM 변환을 위한 객체 (Zone 52로 변경)
         self.utm_converter = Proj(proj="utm", zone=52, ellps="WGS84")
@@ -68,8 +71,9 @@ class StanleyControlNode:
         while not rospy.is_shutdown():
             if self.gps_lat is not None and self.gps_lon is not None:
                 x, y = self.get_cartesian_coords(self.gps_lat, self.gps_lon)
+                self.path_x, self.path_y = self.get_cartesian_coords(self.path_x, self.path_y)
                 delta, _ = self.stanley_control(x, y, self.imu_yaw, self.v, self.path_x, self.path_y)
-                
+                print(delta)
                 delta = max(min(delta, self.max_steering_angle), -self.max_steering_angle)
                 
                 self.pub.publish(delta)
